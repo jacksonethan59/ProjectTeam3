068fa91428f4138d8aa87badfe355daee6156661
jacksonethan59 <97973558+jacksonethan59@users.noreply.github.com>
2022-04-05 14:58:55 -0400

    Update ClassComparison(WIP).py

--- ClassComparison(WIP).py
+++ ClassComparison(WIP).py
@@ -1,2 +1,60 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+class Course(object):
+    def __init__(self, creditHours, preReqsOf):
+        self.creditHours = creditHours
+        self.preReqOf = preReqsOf
+        self.classValue = 0
+    
+    def calculateCourseValue(self):
+        numberOfPreReq = len(self.preReqOf)
+        self.classValue = self.creditHours + numberOfPreReq
+        return self.classValue
+    #defines method to calculate a courses values
+    #simply adds the number of courses which have the given course as preReq to the number of credit hours provided by the course
+    def compareCourseValues(course1, course2):
+        if(course1.classValue > course2.classValue):
+            return course1
+        elif(course1.classValue < course2.classValue):
+            return course2
+        elif(course1.classValue == course2.classValue):
+            return None
+    #defines the method for comparing the value of two courses to determine which one is more "Valuable"
+    #returns the Course object with the greatest courseValue, or None/null in the cases the course are equal
+
+
+
+#Test for when A>B       
+courseA = Course(3,["CPEN3700", "CPEN4700", "CPSC4900"])
+courseB = Course(3,["CPSC3220"])
+
+print("Course A value: ", courseA.calculateCourseValue())
+print("Course B value: ", courseB.calculateCourseValue())
+
+
+if(Course.compareCourseValues(courseA, courseB) == courseA):
+    print("A>B")
+
+
+
+#Test for when A<B       
+courseA = Course(3,["CPSC3220"])
+courseB = Course(3,["CPEN3700", "CPEN4700", "CPSC4900"])
+
+print("Course A value: ", courseA.calculateCourseValue())
+print("Course B value: ", courseB.calculateCourseValue())
+
+if(Course.compareCourseValues(courseA, courseB) == courseB):
+    print("A<B")
+
+
+
+#Test for when A=B       
+courseA = Course(3,["CPEN3700", "CPEN4700", "CPSC4900"])
+courseB = Course(3,["CPEN3700", "CPEN4700", "CPSC4900"])
+
+print("Course A value: ", courseA.calculateCourseValue())
+print("Course B value: ", courseB.calculateCourseValue())
+
+
+if(Course.compareCourseValues(courseA, courseB) == None):
+    print("A=B")
+
