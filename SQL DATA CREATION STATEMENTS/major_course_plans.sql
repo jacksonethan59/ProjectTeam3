a3fc48aac3c49149fb01fbf4eba350bb589816c0
xzl896 <57410670+xzl896@users.noreply.github.com>
2022-04-19 11:47:15 -0400

    Update major_course_plans.sql
    Changed course ID's to valid ID's in the database

--- SQL DATA CREATION STATEMENTS/major_course_plans.sql
+++ SQL DATA CREATION STATEMENTS/major_course_plans.sql
@@ -6,9 +6,9 @@ INSERT INTO schedmaster.major_course_plans (major_id, course_id, min_grade) VALU
 ('CPSC','CPSC1100','C'),
 ('CPSC','CPSC2100','C'),
 ('CPSC','CPSC3200','C'),
-('CPSC','CPSC3500','C'),
-('CPSC','CPSC4200','C'),
-('CPSC','CPSC4500','C');
+('CPSC','CPSC3600','C'),
+('CPSC','CPSC4240','C'),
+('CPSC','CPSC4600','C');
 
 
 
